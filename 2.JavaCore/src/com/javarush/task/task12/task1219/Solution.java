234de4b6bde9eb2374b80686b18f2c25acae8a01
Kir <nomad73@live.ru>
2020-10-12 19:53:18 +0300

    JavaRush

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1219/Solution.java
@@ -0,0 +1,81 @@
+package com.javarush.task.task12.task1219;
+
+/* 
+Создаем человека
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+         void fly();
+    }
+
+    public interface CanRun {
+         void run();
+    }
+
+    public interface CanSwim {
+       void swim();
+    }
+
+
+    public class Human implements CanRun,CanSwim{
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+
+    public class Duck implements CanSwim,CanRun,CanFly{
+
+        @Override
+        public void swim() {
+
+        }
+
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    public class Penguin implements CanRun,CanSwim{
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+
+    public class Airplane implements CanRun,CanFly{
+
+        @Override
+        public void fly() {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
+}
